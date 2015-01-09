50301aa748780cd05efa7b73097952ef3a98657d
Phil Nguyen <pnguyen06@gmail.com>
2015-01-08 21:19:54 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Snapsherpa::Application.config.secret_key_base = '72c7ed005a92d3c284298033a12310f1b08186a135393d15ba8bbe813c70c8b0ef721bd8b9970d870a76eb62765965b262457dea0233be8017b6e87e3e3ec91f'
