6f9b4bf3ca31e0ed48229ae16b4ca2460fe7b58a
Phil Nguyen <pnguyen06@gmail.com>
2015-01-11 16:43:23 -0500

    jquery turbolinks

--- app/assets/javascripts/matcher.js
+++ app/assets/javascripts/matcher.js
@@ -1,3 +1,4 @@
+$(document).ready(function(){
   var substringMatcher = function(strs) {
   return function findMatches(q, cb) {
     var matches, substrRegex;
@@ -35,3 +36,4 @@ $('#the-basics .typeahead').typeahead({
   displayKey: 'value',
   source: substringMatcher(states)
 });
+};
