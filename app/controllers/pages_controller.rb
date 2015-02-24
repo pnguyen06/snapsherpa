bd45591f59624397bae55f16fcd43360e2615cbd
Phil Nguyen <pnguyen06@gmail.com>
2015-02-24 14:58:18 -0500

    Recently added

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   def home
   	@news = News.all.order('date DESC')
   	@brands = Brand.all.order('id ASC')
+  	@recent = Brand.all.order('created_at DESC').paginate(:page => params[:page], :per_page => 6)
   end
 
   def about
